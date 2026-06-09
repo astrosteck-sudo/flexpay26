75b55b94f49050544993778a947460a757863772
Paul Deon Foli <astrosteck@gmail.com>
2026-06-08 20:38:43 -0700

    Added the logout modal and functionality.

--- FlexPay26/src/UserDashBoard/UserDashBoard.jsx
+++ FlexPay26/src/UserDashBoard/UserDashBoard.jsx
@@ -4,14 +4,67 @@ import { getInitials } from "../utils/initials";
 import { SiteFooter } from "../SiteFooter/SiteFooter.jsx";
 import dayjs from "dayjs";
 import "./UserDashBoard.css";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+//import { logout } from '../utils/auth.js'
 
 export function UserDashBoard() {
+  const [logout, setLogOut] = useState(false);
+  const navigate = useNavigate();
   //const token = localStorage.getItem("token");
   const user = JSON.parse(localStorage.getItem("user"));
-  console.log(user);
+  // console.log(user);
+
+  function handleLogOutModal() {
+    if (logout) {
+      setLogOut(false);
+    } else {
+      setLogOut(true);
+    }
+  }
+
+  useEffect(() => {
+    function handleClick(event) {
+      if (
+        !event.target.closest(".user-dashboard-logut-container") &&
+        !event.target.closest(".user-dashboard-header")
+      ) {
+        setLogOut(false);
+      }
+    }
+
+    document.addEventListener("click", handleClick);
+    return () => document.removeEventListener("click", handleClick);
+  }, [setLogOut]);
+
+  function handleLogOut() {
+    console.log("cleio");
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    navigate("/login");
+  }
   return (
     <>
-      <LoginHeader />
+      <div className="user-dashboard-header">
+        <a href="/">
+          <svg
+            className="right-arrow-svg"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <line x1="19" y1="12" x2="5" y2="12" />
+            <polyline points="11 6 5 12 11 18" />
+          </svg>
+        </a>
+
+        <div className="pageheader-site-name">FLEXPAY26</div>
+
+        <div className="user-dashboard-logout" onClick={handleLogOutModal}>
+          Log out
+        </div>
+      </div>
+
       <div className="user-dashBoard-container">
         <div className="user-dashboard-user-info-container">
           <div className="user-dashboard-initials">
@@ -162,8 +215,21 @@ export function UserDashBoard() {
               </p>
             </div>
           </div>
+        </div>
+      </div>
 
-          
+      <div
+        className={`user-dashboard-logout-prompt ${logout ? "open" : "close"}`}
+      >
+        <div className="user-dashboard-logut-container">
+          <p>Log Out</p>
+
+          <p>Are you sure you want to log out?</p>
+
+          <div className="user-dashboard-logut-buttons">
+            <button onClick={() => setLogOut(false)}>Cancel</button>
+            <button onClick={handleLogOut}>Log Out</button>
+          </div>
         </div>
       </div>
 
