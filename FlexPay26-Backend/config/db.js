4aa7ce3418c6fcef8de36e79dd3af18504e6f61c
Paul Deon Foli <astrosteck@gmail.com>
2026-06-08 22:33:44 -0700

    Updated the db.js to suit aiven and render.

--- FlexPay26-Backend/config/db.js
+++ FlexPay26-Backend/config/db.js
@@ -4,7 +4,16 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  connectTimeout: 30000, // 30 seconds
+  // CRITICAL FOR AIVEN:
+  ssl: {
+    rejectUnauthorized: false // Allows connecting safely via SSL without downloading the CA cert file
+  }
 });
 
 module.exports = pool;
