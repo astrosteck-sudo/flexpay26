ac71842a1446107caf72a41299f110aaa1a3ff2f
Paul Deon Foli <astrosteck@gmail.com>
2026-06-11 21:48:28 -0700

    Testing the user dashboard page.

--- FlexPay26/src/axios.js
+++ FlexPay26/src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 //http://localhost:5000
 //https://flexpay26.onrender.com
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://flexpay26.onrender.com/api"
 });
 
 api.interceptors.request.use(
